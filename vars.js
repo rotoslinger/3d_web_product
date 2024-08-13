d19f490777cef8371b4c42625518194879fb6b3b
rotoslinger <harrison.levi.l@gmail.com>
2024-08-13 16:51:27 -0700

    about to change gui and other things

---
+++ vars.js
@@ -0,0 +1,88 @@
+export let gui_map =
+{
+    "mat_attr_dicts": {
+      "screen": {
+        "color": 16777215,
+        "metalness": 0,
+        "roughness": 0.5,
+        "ior": 1.5,
+        "thickness": 0.01,
+        "transmission": 0
+      },
+      "antenna": {
+        "color": 16777215,
+        "metalness": 0,
+        "roughness": 0.5,
+        "ior": 1.5,
+        "thickness": 0.01,
+        "transmission": 0
+      },
+      "glass": {
+        "color": 16777215,
+        "metalness": 0,
+        "roughness": 0.5,
+        "ior": 1.5,
+        "thickness": 0.01,
+        "transmission": 0
+      },
+      "lens": {
+        "color": 16777215,
+        "metalness": 0,
+        "roughness": 0.5,
+        "ior": 1.5,
+        "thickness": 0.01,
+        "transmission": 0
+      },
+      "shiny_black": {
+        "color": 16777215,
+        "metalness": 0,
+        "roughness": 0.5,
+        "ior": 1.5,
+        "thickness": 0.01,
+        "transmission": 0
+      },
+      "tex_metal": {
+        "color": 16777215,
+        "metalness": 0,
+        "roughness": 0.5,
+        "ior": 1.5,
+        "thickness": 0.01,
+        "transmission": 0
+      },
+      "flash_cover": {
+        "color": 16777215,
+        "metalness": 0,
+        "roughness": 0.5,
+        "ior": 1.5,
+        "thickness": 0.01,
+        "transmission": 0
+      },
+      "dir_tex_metal": {
+        "color": 16777215,
+        "metalness": 0,
+        "roughness": 0.5,
+        "ior": 1.5,
+        "thickness": 0.01,
+        "transmission": 0
+      },
+      "metal": {
+        "color": 16777215,
+        "metalness": 0,
+        "roughness": 0.5,
+        "ior": 1.5,
+        "thickness": 0.01,
+        "transmission": 0
+      },
+      "grey_matte": {
+        "color": 16777215,
+        "metalness": 0,
+        "roughness": 0.5,
+        "ior": 1.5,
+        "thickness": 0.01,
+        "transmission": 0
+      }
+    }
+  };
+
+
+export const myArray = [1, 2, 3, 4, 5];
