9261bb4c0f644cee1cffb1fa843fe9a8f54ce3fe
rotoslinger <harrison.levi.l@gmail.com>
2024-08-15 12:00:44 -0700

    Backing up, about to send off to ask apple

--- gui_defaults.js
+++ gui_defaults.js
@@ -1,118 +1,118 @@
 export var gui_map =
 {
-      "screen": {
-        "color": 2631720,
-        "metalness": 0,
-        "roughness": 0,
-        "ior": 1.5,
-        "thickness": 0.01,
-        "transmission": 0
-      },
-      "back": {
-        "color": 13225158,
-        "metalness": 0,
-        "roughness": 0.35,
-        "ior": 2,
-        "thickness": 0,
-        "transmission": 0.84
-      },
-      "antenna": {
-        "color": 12105912,
-        "metalness": 0.57,
-        "roughness": 0.57,
-        "ior": 1.5,
-        "thickness": 0.01,
-        "transmission": 0
-      },
-      "dir_tex_metal": {
-        "color": 13225158,
-        "metalness": 1,
-        "roughness": 0.34,
-        "ior": 0.88,
-        "thickness": 0,
-        "transmission": 0
-      },
-      "shiny_metal": {
-        "color": 16777215,
-        "metalness": 1,
-        "roughness": 0.13,
-        "ior": 1.5,
-        "thickness": 0.01,
-        "transmission": 0
-      },
-        "reg_tex_metal": {
-          "color": 13225158,
-          "metalness": 1,
-          "roughness": 0.43,
-          "ior": 1.5,
-          "thickness": 0.01,
-          "transmission": 0
-      },
-      "glass": {
-        "color": 9671571,
-        "metalness": 0,
-        "roughness": 0,
-        "ior": 1.5,
-        "thickness": 0,
-        "transmission": 1
-      },
-      "lens": {
-        "color": 16777215,
-        "metalness": 0,
-        "roughness": 0.5,
-        "ior": 1.5,
-        "thickness": 0.01,
-        "transmission": 0
-      },
-      "shiny_black": {
-        "color": 1644825,
-        "metalness": 0,
-        "roughness": 0.08,
-        "ior": 1.5,
-        "thickness": 0.01,
-        "transmission": 0
-      },
-      "flash_cover": {
-        "color": 12105912,
-        "metalness": 0,
-        "roughness": 0.5,
-        "ior": 2,
-        "thickness": 0.01,
-        "transmission": 1
-      },
-      "grey_matte": {
-        "color": 8947848,
-        "metalness": 0,
-        "roughness": 0,
-        "ior": 1.5,
-        "thickness": 0.01,
-        "transmission": 0
-      },
-      "black_matte": {
-        "color": 4802889,
-        "metalness": 0,
-        "roughness": 1,
-        "ior": 1.5,
-        "thickness": 0.01,
-        "transmission": 0
-      },
-      "backing_matte": {
-        "color": 16777215,
-        "metalness": 0,
-        "roughness": 0.5,
-        "ior": 1.5,
-        "thickness": 0.01,
-        "transmission": 0
-      },
-      "blocker_matte": {
-        "color": 15527403,
-        "metalness": 1,
-        "roughness": 1,
-        "ior": 1.5,
-        "thickness": 0.01,
-        "transmission": 0
-      }
-  };
+  "screen": {
+    "color": 1184274,
+    "metalness": 0,
+    "roughness": 0,
+    "ior": 1.5,
+    "thickness": 0.01,
+    "transmission": 0
+  },
+  "back": {
+    "color": 13225158,
+    "metalness": 0,
+    "roughness": 0.35,
+    "ior": 2,
+    "thickness": 0,
+    "transmission": 0.84
+  },
+  "antenna": {
+    "color": 12105912,
+    "metalness": 0.57,
+    "roughness": 0.57,
+    "ior": 1.5,
+    "thickness": 0.01,
+    "transmission": 0
+  },
+  "dir_tex_metal": {
+    "color": 13225158,
+    "metalness": 1,
+    "roughness": 0.34,
+    "ior": 0.88,
+    "thickness": 0,
+    "transmission": 0
+  },
+  "shiny_metal": {
+    "color": 16777215,
+    "metalness": 1,
+    "roughness": 0.13,
+    "ior": 1.5,
+    "thickness": 0.01,
+    "transmission": 0
+  },
+  "reg_tex_metal": {
+    "color": 13225158,
+    "metalness": 1,
+    "roughness": 0.43,
+    "ior": 1.5,
+    "thickness": 0.01,
+    "transmission": 0
+  },
+  "glass": {
+    "color": 4671303,
+    "metalness": 0,
+    "roughness": 0,
+    "ior": 1.5,
+    "thickness": 0,
+    "transmission": 0.89
+  },
+  "lens": {
+    "color": 16777215,
+    "metalness": 0,
+    "roughness": 0,
+    "ior": 2,
+    "thickness": 0,
+    "transmission": 1
+  },
+  "shiny_black": {
+    "color": 1644825,
+    "metalness": 0,
+    "roughness": 0.08,
+    "ior": 1.5,
+    "thickness": 0.01,
+    "transmission": 0
+  },
+  "flash_cover": {
+    "color": 16777215,
+    "metalness": 0,
+    "roughness": 0.68,
+    "ior": 2,
+    "thickness": 0,
+    "transmission": 0.76
+  },
+  "grey_matte": {
+    "color": 8947848,
+    "metalness": 0,
+    "roughness": 0,
+    "ior": 1.5,
+    "thickness": 0.01,
+    "transmission": 0
+  },
+  "black_matte": {
+    "color": 2763306,
+    "metalness": 0,
+    "roughness": 1,
+    "ior": 1.5,
+    "thickness": 0.01,
+    "transmission": 0
+  },
+  "backing_matte": {
+    "color": 4605266,
+    "metalness": 0,
+    "roughness": 0.73,
+    "ior": 2,
+    "thickness": 0.01,
+    "transmission": 0
+  },
+  "blocker_matte": {
+    "color": 15527403,
+    "metalness": 1,
+    "roughness": 1,
+    "ior": 1.5,
+    "thickness": 0.01,
+    "transmission": 0
+  }
+};
 
 
 export const myArray = [1, 2, 3, 4, 5];
