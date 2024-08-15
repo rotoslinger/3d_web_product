46124337d3fad2538ea2e615b9e3b4c6e2fa04dd
rotoslinger <harrison.levi.l@gmail.com>
2024-08-14 17:21:27 -0700

    about to update mat defaults

--- gui_defaults.js
+++ gui_defaults.js
@@ -1,25 +1,25 @@
 export var gui_map =
 {
       "screen": {
-        "color": 16777215,
+        "color": 2631720,
         "metalness": 0,
-        "roughness": 0.5,
+        "roughness": 0,
         "ior": 1.5,
         "thickness": 0.01,
         "transmission": 0
       },
       "back": {
-        "color": 12605562,
+        "color": 13225158,
         "metalness": 0,
-        "roughness": 0.32,
+        "roughness": 0.35,
         "ior": 2,
         "thickness": 0,
-        "transmission": 1
+        "transmission": 0.84
       },
       "antenna": {
-        "color": 16777215,
-        "metalness": 0,
-        "roughness": 0.5,
+        "color": 12105912,
+        "metalness": 0.57,
+        "roughness": 0.57,
         "ior": 1.5,
         "thickness": 0.01,
         "transmission": 0
@@ -27,34 +27,34 @@ export var gui_map =
       "dir_tex_metal": {
         "color": 13225158,
         "metalness": 1,
-        "roughness": 0.23,
-        "ior": 1.02,
+        "roughness": 0.34,
+        "ior": 0.88,
         "thickness": 0,
         "transmission": 0
       },
       "shiny_metal": {
         "color": 16777215,
-        "metalness": 0,
-        "roughness": 0.5,
+        "metalness": 1,
+        "roughness": 0.13,
         "ior": 1.5,
         "thickness": 0.01,
         "transmission": 0
       },
-      "reg_tex_metal": {
-        "color": 16777215,
-        "metalness": 0,
-        "roughness": 0.5,
-        "ior": 1.5,
-        "thickness": 0.01,
-        "transmission": 0
+        "reg_tex_metal": {
+          "color": 13225158,
+          "metalness": 1,
+          "roughness": 0.43,
+          "ior": 1.5,
+          "thickness": 0.01,
+          "transmission": 0
       },
       "glass": {
-        "color": 16777215,
+        "color": 9671571,
         "metalness": 0,
-        "roughness": 0.5,
+        "roughness": 0,
         "ior": 1.5,
-        "thickness": 0.01,
-        "transmission": 0
+        "thickness": 0,
+        "transmission": 1
       },
       "lens": {
         "color": 16777215,
@@ -65,30 +65,38 @@ export var gui_map =
         "transmission": 0
       },
       "shiny_black": {
-        "color": 16777215,
+        "color": 1644825,
         "metalness": 0,
-        "roughness": 0.5,
+        "roughness": 0.08,
         "ior": 1.5,
         "thickness": 0.01,
         "transmission": 0
       },
       "flash_cover": {
-        "color": 16777215,
+        "color": 12105912,
         "metalness": 0,
         "roughness": 0.5,
-        "ior": 1.5,
+        "ior": 2,
         "thickness": 0.01,
-        "transmission": 0
+        "transmission": 1
       },
       "grey_matte": {
-        "color": 16777215,
+        "color": 8947848,
         "metalness": 0,
-        "roughness": 0.5,
+        "roughness": 0,
         "ior": 1.5,
         "thickness": 0.01,
         "transmission": 0
       },
       "black_matte": {
+        "color": 4802889,
+        "metalness": 0,
+        "roughness": 1,
+        "ior": 1.5,
+        "thickness": 0.01,
+        "transmission": 0
+      },
+      "backing_matte": {
         "color": 16777215,
         "metalness": 0,
         "roughness": 0.5,
@@ -97,9 +105,9 @@ export var gui_map =
         "transmission": 0
       },
       "blocker_matte": {
-        "color": 16777215,
-        "metalness": 0,
-        "roughness": 0.5,
+        "color": 15527403,
+        "metalness": 1,
+        "roughness": 1,
         "ior": 1.5,
         "thickness": 0.01,
         "transmission": 0
